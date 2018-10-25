ba47e01992954c4a74448508f1f90f24d5cfd038
sergiqz <37753348+sergiqz@users.noreply.github.com>
2018-10-25 15:00:34 -0500

    Add files via upload

---
+++ bh/maxbh.cpp
@@ -0,0 +1,131 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+template <class T, size_t cap = 100>
+class BH
+{
+    private:
+        T * elems;
+        size_t size;
+        int capacidad=0;
+        
+    public:
+        BH(): elems(new T[cap]), size(0){};
+        ~BH()
+        {
+            delete [] elems;
+        }
+        
+        void push(T x)
+        {
+            if(capacidad == cap){
+                return;
+            } 
+            elems[capacidad] = x;  
+            up(capacidad);
+            capacidad++;
+        }
+        
+        // delete min 
+        void pop()
+        {
+            if(capacidad==0){
+                return;
+            }
+            
+            elems[0]=elems[capacidad-1];
+            down(0);
+            capacidad--;
+        }
+        
+        // min 
+        T top()
+        {
+            return elems[0];
+        }
+
+        void Heapily(){
+            for(int i=cap/2;i>=0;i--){
+                down(i);
+            }
+        }
+
+
+    
+    private:
+        void up(int i)
+        {
+            while (i != 0 and elems[father(i)] < elems[i]) // i != 0 xq no puede subir la raiz y su padre tiene q ser mayor
+            {  
+                swap(elems [i], elems[father(i)]);
+                i = father(i);
+            }
+        }
+
+        
+        void down(int i)
+        {
+            while (i<capacidad){ 
+            if(max(i)){
+                swap(elems[i],elems[childrig(i)]);
+                i=childrig(i);
+            }
+            else if(max(i)==false){
+                swap(elems[i],elems[childlef(i)]);
+                i=childlef(i);
+            }
+            else{       
+                break;
+            }
+          } 
+            
+        }
+        int father(int i){
+            return (i-1)/2;
+        }
+        int childlef(int i){
+            return i*2+1; 
+        } 
+        int childrig(int i){
+            return i*2+2;
+        }
+        bool max (T i){
+            if(elems[childrig(i)]>elems[childlef(i)]){
+                return true;
+            }else{
+                return false;
+            }
+
+        }
+
+        void Heap_sort(){
+
+        }
+
+};
+
+int main()
+{
+    BH<int> h;
+    
+    for(int i = 19; i > 9; i--){
+        h.push(i);
+    }
+    
+    int array[10];
+
+
+
+    cout << h.top() << endl;
+    h.pop();
+    
+    cout << h.top() << endl;
+
+    h.Heapily();
+    cout << h.top() << endl;
+
+    return 0;
+}

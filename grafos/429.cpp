6e1d2bd6105ae0973a8135a1a47c4d6ca62fe549
sergiqz <37753348+sergiqz@users.noreply.github.com>
2018-11-09 22:14:47 -0500

    Add files via upload

--- grafos/429.cpp
+++ grafos/429.cpp
@@ -4,71 +4,39 @@
 #include <sstream>
 #include <vector>
 #include <queue>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 vector<string> words;
-map<string, int> visitado;
-
-
-void bfs(string u){
-	queue<string> q;
-    visitado[u] = 0;
-    q.push(u);
-    while (!q.empty()){
-        u = q.front();
-        q.pop();
-        for (int i = 0; i < words.size(); ++i){
-                    if (! (visitado.count(words[i]) )){
-                        int cont = 0;
-                        for (int j = 0; j < u.length(); ++j)
-                            if (u[j] != words[i][j]){
-                                ++cont;
-                            }
-                        if (cont == 1){
-                            visitado[words[i]] = visitado[u] + 1;
-                            q.push(words[i]);
-                        }
-                        
-                    }
-                    
-                }
-            }
-                
-}
-
-
-
-
-
-
-
-
-
 
 int main()
 {  
     int n;
     cin >> n;
-    while ( n-- ){
+    while (n--){
         string word;
         while(word != "*"){
             cin >> word,
             words.push_back(word);
         }
         cin.ignore();
+
         string line;
         while (getline(cin, line) and line != "")
         {
             stringstream ss(line);
-            string x, y;
-            ss >> x >> y;
+            string u, t;
+            ss >> u >> t;
             queue<string> q;
-            visitado[x] = 0;
-            q.push(x);
-            while (!q.empty()){
+            map<string, int> visitado;
+            visitado[u] = 0;
+            q.push(u);
+            while (!q.empty() and  visitado.count(t) == 0){
+                string x;
                 x = q.front();
                 q.pop();
-            for (int i = 0; i < words.size(); ++i){
-                    if (! (visitado.count(words[i]) )){
+                for (int i = 0; i < words.size(); ++i){
+                    if (! (visitado.count(words[i]) and u.length() == words[i].length())){
                         int cont = 0;
                         for (int j = 0; j < x.length(); ++j)
                             if (x[j] != words[i][j]){
@@ -82,15 +50,52 @@ int main()
                     }
                     
                 }
-            }    
-            cout<<x<<" "<<y<<" "<<visitado[y]<<endl;
+            }
+             
+
+            cout << u << " " << t << " " << visitado[t] << endl;
         }
         
-    }
-    if (n){
     
-    cout << endl;
-    
+        if (n){
+        
+        cout << endl;
+        
+        }
     }
+
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
